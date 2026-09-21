b310416031a9b2c560be6c6dbcabb88e3160f282
svyshnavi1450-lab <svyshnavi1450@gmail.com>
2026-09-21 14:58:09 +0530

    Time: 2121 ms (9.64%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,9 @@
+
+select s.name
+from salesperson as s 
+left join orders as o 
+on s.sales_id=o.sales_id
+left join company as c on o.com_id=c.com_id
+group by s.name
+having sum(c.name="RED")=0
+or sum(c.name="RED") is null;
